a71930c3783dd2a3265ac73e2e1e533035e74024
Akbar Gunadi <gunadiakbar24@gmail.com>
2022-03-11 22:17:27 +0700

    create database

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 module.exports = {
   development: {
-    username: process.env.DB - USERNAME,
-    password: process.env.DB - PASSWORD,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
     database: 'coding_practice',
     host: '127.0.0.1',
     dialect: 'postgres',
